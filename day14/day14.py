611fc1cf62391b0af7daa406befe1ec84ee02479
David Sanchez Arocha <davidsa@themediapeople.co.uk>
2024-12-17 16:40:08 +0000

    day14 part 2

--- day14/day14.py
+++ day14/day14.py
@@ -137,3 +137,51 @@ part1_sol = solve_part1('day14/input.txt', 100, 101, 103)
 elapsed_time = timeit.default_timer()-start
 print(f'Day 14 Part 1 Solution = {part1_sol}')
 print(f'Day 14 Part 1 Run Time = {str(elapsed_time)}')
+
+
+
+
+
+## ----- PART 2 ----- ##
+
+import sys
+import time
+
+W = 101
+H = 103
+
+ans = [0,0,0,0]
+
+robots = []
+
+st = 0
+for line in open("day14/input.txt"):
+    if line.strip() == "":
+        continue
+    p,v = line.split()
+    px,py = map(int,p[2:].split(",")) 
+    vx,vy = map(int,v[2:].split(","))
+    robots.append(((px,py),(vx,vy)))
+
+seconds = 0
+while True:
+    grid = [[0 for _ in range(W)] for _ in range(H)]
+    seconds += 1
+
+    bad = False
+    for robot in robots:
+        pr1,pr2 = robot
+        px,py = pr1
+        vx,vy = pr2
+        nx,ny = px + seconds*vx, py + seconds*vy
+        nx = nx % W
+        ny = ny % H
+        grid[ny][nx] += 1
+        if grid[ny][nx] > 1:
+            bad = True
+
+    if not bad:
+        print(seconds)
+        # for row in grid:
+        #     print("".join(map(str,row)))
+        # time.sleep(0.3)
